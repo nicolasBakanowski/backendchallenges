06806f489e64d553ee2bd9b769abb574f9a9dff6
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-02 16:49:54 -0300

    enviroment variable

--- myApp.js
+++ myApp.js
@@ -15,6 +15,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/json',function(req,res){
+    console.log(process.env.MESAGGE_STYLE)
     res.json({
         "message":process.env.MESAGGE_STYLE
     });
