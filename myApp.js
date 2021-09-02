f09b3e46399ea76f09bcb09c419f523c3698f710
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-02 15:28:06 -0300

    statics

--- myApp.js
+++ myApp.js
@@ -3,11 +3,14 @@ const { allowedNodeEnvironmentFlags } = require('process');
 var app = express();
 
 pathtoTemplate = __dirname + '/views/index.html'
+pathtoPublic = __dirname +'/public'
+
+
 app.get('/',function(req,res){
     res.sendFile(pathtoTemplate);
 });
 
-
+app.use('/public',express.static(pathtoPublic));
 
 
 
