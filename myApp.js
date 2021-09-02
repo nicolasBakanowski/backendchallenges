bc2cb0bf6e0309878554c9738ac17104c25fc37b
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-01 22:16:03 -0300

    index.html2

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ var express = require('express');
 const { allowedNodeEnvironmentFlags } = require('process');
 var app = express();
 
-pathtoTemplate = __dirname + 'views/index.html'
+pathtoTemplate = __dirname + '/views/index.html'
 app.get('/',function(req,res){
-    res.send(pathtoTemplate);
+    res.sendFile(pathtoTemplate);
 });
 
 
