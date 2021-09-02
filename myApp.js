cda58a1b92b5cdc7d59a22c0b94d9241f8237061
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-02 16:28:02 -0300

    try static

--- myApp.js
+++ myApp.js
@@ -5,14 +5,20 @@ var app = express();
 pathtoTemplate = __dirname + '/views/index.html'
 pathtoPublic = __dirname +'/public'
 
-
+// this way, can use the files into public dir..css anywhere
 app.use('/public',express.static('public'));
+
+
 
 app.get('/',function(req,res){
     res.sendFile(pathtoTemplate);
 });
 
-
+app.get('/json',function(req,res){
+    res.json({
+        "message":"hello json"
+    });
+})
 
 
 
