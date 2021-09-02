aeac9de6efbc6979df4887554a9c4df91a569698
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-02 20:13:53 -0300

    fix problem

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ var express = require('express');
 const { allowedNodeEnvironmentFlags } = require('process');
 var app = express();
 require('dotenv').config()
-process.env.MESSAGE_STYLE='uppercase';
 
 pathtoTemplate = __dirname + '/views/index.html'
 pathtoPublic = __dirname +'/public'
@@ -16,18 +15,20 @@ app.get('/',function(req,res){
     res.sendFile(pathtoTemplate);
 });
 
-app.get('/json',function(req,res){
-let response ={
-    "mesagge":"Hello Json"
-};   
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-     response.mesagge=response.mesagge.toUpperCase();
-    }
-return res.json(response);    
-})
 
+app.get('/json', function(req, res){
+// Variable assignment as object
+var response = {
+  "message": "Hello json"
+};
 
+if(process.env.MESSAGE_STYLE==='uppercase'){
+  //Override message attribute value based on condition
+  response.message = response.message.toUpperCase();  
+}
 
+return res.json(response);
+});
 
 
 
