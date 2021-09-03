729c405c20db4c8c3bea532eb6c78be88154da51
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-02 23:03:10 -0300

    fix problem

--- myApp.js
+++ myApp.js
@@ -9,12 +9,19 @@ pathtoPublic = __dirname +'/public'
 // this way, can use the files into public dir..css anywhere
 app.use('/public',express.static('public'));
 
+app.use(function(req,res,next){
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string)
+    next()
 
+})
+ 
 
-app.get('/',function(req,res){
+app.get('/',function(req,res,next){
+   
     res.sendFile(pathtoTemplate);
+    next()
 });
-
 
 app.get('/json', function(req, res){
 // Variable assignment as object
