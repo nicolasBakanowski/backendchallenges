cfeba068574d4b805e579c2b0ce4309cb3525156
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-04 14:33:25 -0300

    urlquery

--- myApp.js
+++ myApp.js
@@ -52,7 +52,16 @@ app.get('/:word/echo',function(req,res){
   })
 })
 
-
+app.get('/name',function(req,res){
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 
 
 
