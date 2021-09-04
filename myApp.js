c4621410712ab68abc008cb2e265654ac907f13a
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-09-04 00:53:10 -0300

    variables por patch

--- myApp.js
+++ myApp.js
@@ -45,6 +45,12 @@ app.get('/now', function(req,res,next){
   return res.send({"time": req.time})
 })
 
+app.get('/:word/echo',function(req,res){
+  const {word} = req.params;
+  res.json({
+    echo: word
+  })
+})
 
 
 
